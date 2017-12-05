97c98cef3599498751cb4ae7edf13528e0cf8a3d
hjzheng <zhenghaoju700@163.com>
2017-12-05 14:46:21 +0800

    first commit

---
+++ router/mock-conf.js
@@ -0,0 +1,23 @@
+var argv = require('yargs').argv;
+var fs = require('fs');
+var path = require('path');
+var toString = Object.prototype.toString;
+var configurations = {
+	list: [],
+	add: function(config) {
+		if (toString.call(config) === '[object Object]') {
+			this.list.push(config);
+		}
+		if (toString.call(config) === '[object Array]') {
+			this.list = this.list.concat(config);
+		}
+	}
+};
+
+if(argv.mock) {
+	fs.readdirSync(argv.mock).forEach(function (file) {
+		require(path.join(process.cwd(), argv.mock, file))(configurations);
+	});
+}
+
+module.exports = configurations.list;
